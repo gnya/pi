c911333f99e2ca3d90f405960c00d18de262dcda
gnya <gnya.motion@gmail.com>
2026-04-27 02:21:50 +0900

    ex_10.py あれこれ試して諦めた

--- ex_10.py
+++ ex_10.py
@@ -1,70 +1,76 @@
 # 円周率を10^8桁計算してみる（チュドノフスキー・アルゴリズム）
 # 参考: https://bellard.org/pi/pi2700e9/pipcrecord.pdf
+import math
 from multiprocessing import Pool
 from typing import Callable
 
 import gmpy2
 from gmpy2 import mpfr, mpz
 
-from utils import check_pi, split_range, timeit
+from utils import split_range, timeit
 
 PQT = tuple[mpz, mpz, mpz]
 
 
 class PiCalculator:
+    BITS_PER_DIGIT = 3.32192809488736234787
     DIGITS_PER_ITER = 14.1816474627254776555
 
-    CONST_A = 545140134
-    CONST_B = 13591409
-    CONST_C = int(640320**3 / 24)
-    CONST_D = int(640320**3 / 16)
+    CONST_A = mpz(545140134)
+    CONST_B = mpz(13591409)
+    CONST_C = mpz(640320**3 / 24)
+    CONST_D = mpz(640320**3 / 144)
 
-    def __init__(self, digits: int, n_jobs: int = 8, n_merge_jobs: int = 2) -> None:
+    def __init__(self, depth: int, n_jobs: int = 8, n_merge_jobs: int = 2) -> None:
         if n_jobs < 1:
             raise RuntimeError("n_jobsには1以上の値を設定してください")
 
-        if n_merge_jobs < 1:
+        min_depth = 0 if n_jobs == 1 else (n_jobs - 2).bit_length()
+
+        if depth < min_depth:
+            raise RuntimeError(f"depthには{min_depth}以上の値を設定してください")
+        elif n_merge_jobs < 1:
             raise RuntimeError("n_merge_jobsには1以上の値を設定してください")
         elif n_merge_jobs > n_jobs:
             raise RuntimeError("n_merge_jobsにはn_jobs以下の値を設定してください")
 
-        self.digits = digits
+        self.max_loop = 1 + (1 << depth)
         self.n_jobs = n_jobs
         self.n_merge_jobs = n_merge_jobs
 
-        self.max_loop = int(digits / self.DIGITS_PER_ITER) + 5
-        self.precision = self.digits * 5
+        digits = self.max_loop * self.DIGITS_PER_ITER
+        self.precision = int(math.ceil(digits * self.BITS_PER_DIGIT))
 
-        self._pqt_list = []
+        self._pqt_list: list[PQT] = []
 
     # この関数はアルゴリズムのn項目のPQTを返します
-    def pqt_term(self, n: int) -> PQT:
-        a = mpz((self.CONST_A * n + self.CONST_B))
-        p = mpz((2 * n - 1) * (6 * n - 1) * (6 * n - 5))
-        q = mpz(self.CONST_C * n**3)
+    def pqt_term(self, n: mpz) -> PQT:
+        a = self.CONST_A * n + self.CONST_B
+        p = (2 * n - 1) * (6 * n - 1) * (6 * n - 5)
+        q = self.CONST_C * n**3
 
         if n % 2 == 0:
             t = a * p
         else:
             t = -a * p
 
-        return (p, q, t)
+        return (p, q, t)  # type: ignore
 
     # この関数はn要素目の計算済みPQTを返します
-    def pqt_list(self, n: int) -> PQT:
-        return self._pqt_list[n]
+    def pqt_list(self, n: mpz) -> PQT:
+        return self._pqt_list[int(n)]
 
     # この関数はアルゴリズムのPQTの合成を行います
-    def merge_pqt(self, pqt_l: PQT, pqt_r: PQT) -> PQT:
+    def merge_pqt(self, a: PQT, b: PQT) -> PQT:
         return (
-            pqt_l[0] * pqt_r[0],
-            pqt_l[1] * pqt_r[1],
-            pqt_l[2] * pqt_r[1] + pqt_r[2] * pqt_l[0],
+            a[0] * b[0],
+            a[1] * b[1],
+            a[2] * b[1] + b[2] * a[0],
         )
 
     # この関数は2分法でアルゴリズムのPQTを計算します
     # NOTE 範囲はstart項目からend-1項目までになります
-    def calc_pqt(self, start: int, end: int, term: Callable[int, PQT]) -> PQT:
+    def calc_pqt(self, start: mpz, end: mpz, term: Callable[mpz, PQT]) -> PQT:
         if end <= 0 or start > end - 1:
             raise RuntimeError("endとstartの値が正しくありません:", start, end)
         elif start == end - 1:
@@ -82,12 +88,12 @@ class PiCalculator:
     def calc_all_pqt(self) -> PQT:
         with Pool(self.n_jobs) as pool:
             args = split_range(1, self.max_loop, self.n_jobs)
-            args = [(s, e, self.pqt_term) for s, e in args]
+            args = [(mpz(s), mpz(e), self.pqt_term) for s, e in args]
             self._pqt_list = pool.starmap(self.calc_pqt, args)
 
         with Pool(self.n_merge_jobs) as pool:
             args = split_range(0, self.n_jobs, self.n_merge_jobs)
-            args = [(s, e, self.pqt_list) for s, e in args]
+            args = [(mpz(s), mpz(e), self.pqt_list) for s, e in args]
             self._pqt_list = pool.starmap(self.calc_pqt, args)
 
         return self.calc_pqt(0, len(self._pqt_list), self.pqt_list)
@@ -95,39 +101,31 @@ class PiCalculator:
     # この関数は円周率を計算します
     @timeit
     def calc_pi(self) -> mpfr:
-        _, num, t = self.calc_all_pqt()
-        den = mpz(3) * (t + num * mpz(self.CONST_B))
-
         with gmpy2.context(precision=self.precision):
-            # 逆数平方根のほうが速い
-            pi = num / (den * gmpy2.rec_sqrt(mpz(self.CONST_D)))
+            _, num, den = self.calc_all_pqt()
+            pi = num / ((den + num * self.CONST_B) * gmpy2.rec_sqrt(self.CONST_D))
 
         return pi
 
 
 if __name__ == "__main__":
-    import ex_7
+    import os
 
-    n = 10**7
-    calc = PiCalculator(n)
+    k = 23
+    calc = PiCalculator(k)
 
-    """
-    timer = time_start("pqt_term (ex_10)")
-    for i in range(n):
-        calc.pqt_term(i + 1)
-    timer.stop()
-
-    timer = time_start("pqt_term (ex_7)")
-    for i in range(n):
-        ex_7.pqt_term(i + 1)
-    timer.stop()
-    """
-
-    # 古い方の実装
-    ex_7.calc_pi_old(n, 8)
+    print("digits:", int(2**k * calc.DIGITS_PER_ITER))
 
     # 1億桁の計算に40秒ほどかかる
     pi = calc.calc_pi()
+    str_pi = str(pi)
 
-    # 今の方法だと500万桁を超える円周率をチェックできない…
-    print(f"digits: {check_pi(pi)}")
+    path = f"{os.path.dirname(__file__)}\\pi_{k}.txt"
+
+    with open(path, "w") as f:
+        # 計算した円周率をテキストに書き出す
+        f.write(str_pi)
+
+    # 最後から20桁を表示する
+    # TODO 500万桁を超える円周率をチェックできるようにする
+    print(f"pi {len(str_pi) - 1 - 20}-{len(str_pi) - 1 - 1}:", str_pi[-20:])
